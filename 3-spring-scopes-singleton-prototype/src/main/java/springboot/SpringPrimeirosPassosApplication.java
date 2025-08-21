db1814905314627b48ee608fee6e20c2c48ba4ed
Ariel França <ariel.franca@hotmail.com>
2025-08-21 14:52:08 -0300

    Add Spring Boot application with prototype and singleton scopes, including configuration for beans, messaging system, and initial test setup.

---
+++ 3-spring-scopes-singleton-prototype/src/main/java/springboot/SpringPrimeirosPassosApplication.java
@@ -0,0 +1,26 @@
+package springboot;
+
+import springboot.app.SistemaMensagem;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringPrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(SistemaMensagem sistema) throws Exception {
+		return args -> {
+			sistema.enviarConfirmacaoCadastro();
+			System.out.println("=============");
+			sistema.enviarMensagemBoasVindas();
+			System.out.println("=============");
+			sistema.enviarConfirmacaoCadastro();
+		};
+	}
+
+}
---
+++ 3-spring-scopes-singleton-prototype/src/main/java/springboot/app/SistemaMensagem.java
@@ -0,0 +1,24 @@
+package springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem {
+    @Autowired
+    private Remetente noreply;
+    @Autowired
+    private Remetente techTeam;
+
+    public void enviarConfirmacaoCadastro() {
+        System.out.println(noreply);
+        System.out.println("Seu cadastro foi aprovado");
+    }
+
+    public void enviarMensagemBoasVindas() {
+        techTeam.setNome("Ariel");
+        techTeam.setEmail("ariel@teste.com");
+        System.out.println(techTeam);
+        System.out.println("Bem-vindo à Tech Elite");
+    }
+}
